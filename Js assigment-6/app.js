6260e306aa568355fc71c117bf7e219ece98597d
Saima Imran <smalldeeds1234@gmail.com>
2024-08-30 00:59:16 +0500

    Add files via upload

---
+++ Js assigment-6/app.js
@@ -0,0 +1,120 @@
+// Assigement 6 Q1//
+
+
+// var x=20; 
+// ++x;
+// document.write(x)
+
+// var x=21;
+// x++;
+// document.write(x)
+
+// var x=22;
+// --x;
+// document.write(x)
+
+// var x= 21;
+// x--;
+// document.write(x)
+
+// document.write("<h1>RESULT</h1>")
+
+// document.write("The value of x is:20</br>")
+
+// document.write("The value of ++x is 21</br>")
+// document.write("Now the value of x is:21</br>")
+
+// document.write("The value of x++ is 22</br>")
+// document.write("Now the value of x is:22</br>")
+
+// document.write("The value of --x is 21</br>")
+// document.write("Now the value of x is:21</br>")
+
+// document.write("The value of x-- is 20</br>")
+// document.write("Now the value of x is:20</br>")
+
+// --------------------------------------------------------------------------------------------//
+
+// Assigement #6 Q2//
+
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write(result)
+// document.write("The var result of a and b is:3</br>")
+
+// var a=2
+// var ans= --a;
+// document.write(ans)
+// document.write("--a is 1")
+
+// var ans=--a - --b;
+// document.write(ans)
+// document.write("--a - --b is 0")
+
+
+// var ans=--a - --b + ++b;
+// document.write(ans)
+// document.write("--a - --b + ++b is 2")
+
+
+
+// var ans=--a - --b + ++b + b--;
+// document.write(ans)
+// document.write("--a - --b + ++b + b-- is 3")
+// ---------------------------------------------------------------------------------------------------
+// Assigement 6 Q3//
+
+// var userName= prompt("What is your name?")
+// document.write("Welcome"+" "+userName+", to our webpage")
+
+// ---------------------------------------------------------------------------------
+
+//  Assigement 6 Q4//
+
+// var num=prompt("Write a number for instant table");
+
+//     document.write("<h1>Table of 5</h1>")
+
+//     document.write(5+"X"+"1"+"="+5*1+"</br>")
+//     document.write(5+"X"+"2"+"="+5*2+"</br>")
+//     document.write(5+"X"+"3"+"="+5*3+"</br>")
+//     document.write(5+"X"+"4"+"="+5*4+"</br>")
+//     document.write(5+"X"+"5"+"="+5*5+"</br>")
+//     document.write(5+"X"+"6"+"="+5*6+"</br>")
+//     document.write(5+"X"+"7"+"="+5*7+"</br>")
+//     document.write(5+"X"+"8"+"="+5*8+"</br>")
+//     document.write(5+"X"+"9"+"="+5*9+"</br>")
+//     document.write(5+"X"+"10"+"="+5*10+"</br>")
+   
+    // -------------------------------------------------------------------------------//
+
+    //  Assigement 6 Q5 (a)//
+
+//     var subject1=("Science")
+//     var subject2=("Math")
+//     var subject3=("Computer")
+
+//     var total_marks1=(100)
+//     var total_marks2=(100)
+//     var total_marks3=(100)
+//     var Total=(total_marks1+total_marks2+total_marks3)
+
+
+// var obtained_marks1=(60)
+// var obtained_marks2=(60)
+// var obtained_marks3=(50)
+//   var final=(obtained_marks1+obtained_marks2+obtained_marks3)
+ 
+
+//     var obtained_marks1=prompt("Write obtained marks")
+//     var obtained_marks2=prompt("Write obtained marks")
+//     var obtained_marks3=prompt("Write obtained marks")
+//     var last=prompt("Average")
+// document.write("<b>Subject TotalMarks ObtainedMarks Percentage</b> </br>")
+
+// document.write(subject1+" "+total_marks1+"   "+obtained_marks1+"  "+obtained_marks1/total_marks1*100+"%"+"</br>")
+// document.write(subject2+"  "+total_marks2+"  "+obtained_marks2+"   "+obtained_marks2/total_marks2*100+"%"+"</br>")
+// document.write(subject3+"  " +total_marks3+"  " +obtained_marks3+"  "+obtained_marks3/total_marks3*100+"%"+"</br>"+"</br>")
+// document.write(Total+" "+final+"  "+final/Total*100+"%")
+
+
